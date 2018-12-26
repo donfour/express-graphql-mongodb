3ca974ff351c2a717b543c94f398e8c16110da9a
Donovan So <donovanso@yahoo.com.hk>
2018-12-26 14:23:38 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+// module dependencies
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const MongoClient = require('mongodb').MongoClient;
+const cors = require('cors');
+
+// constants
+const MONGODB_URL = process.env.MONGODB_URL;
+const PORT = 4000;
+
+// setup
+const app = express();
+
+// allow cors
+app.use(cors());
+
+app.use('/graphql', graphqlHTTP(req => ({
+    schema,
+    graphiql: true, // enable GUI for testing graphql server
+    rootValue: { db: req.app.locals.db } // inject db into parent object in the resolve function
+})));
+
+MongoClient.connect(MONGODB_URL) // connect to db, then start server
+  .catch(err => console.error(err.stack))
+  .then(client => {
+    app.locals.db = client.db('database_name');
+    app.listen(PORT, () => {
+        console.log(`Now listening for requests on port ${PORT}`);
+     });
+  });
